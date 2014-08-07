ee12b14db0f75138c11da81dce419a862e1bff2a
Shane M. Clements <shanemc@media.mit.edu>
2014-08-07 09:56:45 -0400

    Added browser extension

---
+++ browser_extension.js
@@ -0,0 +1,45 @@
+// Scratch Extension to demonstrate some simple web browser functionality
+// 2014 Shane M. Clements
+
+(function(ext) {
+    ext.alert = function(message) {
+        alert(message);
+    };
+
+    ext.confirm = function(question) {
+        return confirm(question);
+    };
+
+    ext.ask = function(question) {
+        return prompt(question);
+    };
+
+    ext.setTitle = function(title) {
+        window.document.title = title;
+    };
+
+    ext.openTab = function(location) {
+        window.open(location, '_blank');
+    };
+
+    ext._shutdown = function() {
+        console.log('Shutting down...');
+    };
+
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    var descriptor = {
+        blocks: [
+            [' ', 'alert %s', 'alert', ''],
+            ['r', 'confirm %s', 'confirm', 'Are you sure?'],
+            ['r', 'ask %s', 'ask', 'How are you?'],
+            [' ', 'set window title to %s', 'setTitle', 'title'],
+            [' ', 'open tab with %s', 'openTab', 'https://twitter.com/scratchteam']
+        ]
+    };
+
+    ScratchExtensions.register('Browser Stuff', descriptor, ext);
+})({});
+
